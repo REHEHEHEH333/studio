a783f691f37c9da0d871b12baa6655b48d128703
REHEHEHEH333 <garrykod@gmail.com>
2025-08-03 17:02:59 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
-  basePath: '',
-  distDir: '.next',
   images: {
     unoptimized: true, // Required for static exports
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  // Add any other configurations you need here
+  // Add other configurations as needed
 };
 
 export default nextConfig;
